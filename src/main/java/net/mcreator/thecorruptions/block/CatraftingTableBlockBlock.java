0d4fa785d9a45811c892dd279e1f37f5fae4bfdf
Wat Studios <159730338+WatStudios@users.noreply.github.com>
2025-07-11 13:32:01 -0500

    edit

--- src/main/java/net/mcreator/thecorruptions/block/CatraftingTableBlockBlock.java
+++ src/main/java/net/mcreator/thecorruptions/block/CatraftingTableBlockBlock.java
@@ -1,6 +1,9 @@
 
 package net.mcreator.thecorruptions.block;
 
+import net.minecraftforge.network.NetworkHooks;
+
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -10,11 +13,21 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.MenuProvider;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.Containers;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.core.BlockPos;
 
+import net.mcreator.thecorruptions.world.inventory.CatraftingtableMenu;
 import net.mcreator.thecorruptions.block.entity.CatraftingTableBlockBlockEntity;
+
+import io.netty.buffer.Unpooled;
 
 public class CatraftingTableBlockBlock extends Block implements EntityBlock {
 	public CatraftingTableBlockBlock() {
@@ -24,6 +37,25 @@ public class CatraftingTableBlockBlock extends Block implements EntityBlock {
 	@Override
 	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
 		return 15;
+	}
+
+	@Override
+	public InteractionResult use(BlockState blockstate, Level world, BlockPos pos, Player entity, InteractionHand hand, BlockHitResult hit) {
+		super.use(blockstate, world, pos, entity, hand, hit);
+		if (entity instanceof ServerPlayer player) {
+			NetworkHooks.openScreen(player, new MenuProvider() {
+				@Override
+				public Component getDisplayName() {
+					return Component.literal("Catrafting Table Block");
+				}
+
+				@Override
+				public AbstractContainerMenu createMenu(int id, Inventory inventory, Player player) {
+					return new CatraftingtableMenu(id, inventory, new FriendlyByteBuf(Unpooled.buffer()).writeBlockPos(pos));
+				}
+			}, pos);
+		}
+		return InteractionResult.SUCCESS;
 	}
 
 	@Override
